87ebc59243f2732072dfd7d199b98b3aa579e5cd
Shannon Millar <shanenak@gmail.com>
2023-10-03 17:42:44 -0700

    Do validations for user model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :email, 
+    uniqueness: true, 
+    length: { in: 3..255 }, 
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { in: 6..255 }, allow_nil: true
+end
