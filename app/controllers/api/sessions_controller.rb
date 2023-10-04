f3c4a3e8a1760f81f8b17c518bb52cc4f3929313
Shannon Millar <shanenak@gmail.com>
2023-10-03 22:01:22 -0700

    Setup CSRF and error handling

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,8 @@
 class Api::SessionsController < ApplicationController
   def show
     if current_user
-      render json: { user: current_user}
+      @user = current_user
+      render 'api/users/show'
     else
       render json: { user: nil }
     end
@@ -11,7 +12,7 @@ class Api::SessionsController < ApplicationController
     @user = User.find_by_email(params[:email], params[:password])
     if @user
       login!(@user)
-      render json: { user: @user }
+      render 'api/users/show'
     else
       render json: { errors: ['The provided credentials were invalid.']}, status: 422
     end
