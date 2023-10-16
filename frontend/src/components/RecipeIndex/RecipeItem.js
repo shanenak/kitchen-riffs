d3b8cd317bd15e60ce4289a4aa7029afc03261c3
Shannon Millar <shanenak@gmail.com>
2023-10-16 10:48:19 -0700

    Add link to full index item

--- frontend/src/components/RecipeIndex/RecipeItem.js
+++ frontend/src/components/RecipeIndex/RecipeItem.js
@@ -1,13 +1,17 @@
-import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import { NavLink, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function RecipeItem ({recipe}) {
+    const history = useHistory()
+    const saved = window.location.pathname === '/saved'
+    console.log(saved)
+
     const time = recipe.timeRequired.replace(/[^0-9]/g, '');
     const quickTag = (<div id='tag'>
                     <p>Quick</p>
                 </div>)
     const tagInclude = (time<30)&(time>=5) ? quickTag : <></>;
     return(
-        <div id='recipe-item' key={recipe.id}>
+        <div id='recipe-item' key={recipe.id} onClick={()=>history.push(`/recipes/${recipe.id}`)}>
             <div id='grid-image'>
                 <img src={recipe.photoUrl} alt='recipe-result'></img>
                 {tagInclude}
